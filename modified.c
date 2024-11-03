321ed7ba8bfd5d8f558b6e903cf97cf7a4a99a71
Chris <comosjay@gmail.com>
2024-11-03 01:38:21 -0800

    Done

--- modified.c
+++ modified.c
@@ -23,13 +23,16 @@ int main(int argc, char *argv[]) {
         	fprintf(stderr, "Error opening pcap file: %s\n", errbuf);
 		return 1;
 	}
-	while ((packet = pcap_next(handle, &header)) != NULL) {
-//		if(header.caplen < sizeof(struct ethhdr) + sizeof(struct iphdr)){
-			
-//		}
+	while ((packet = pcap_next(handle, &header)) != NULL){
 		ip_header = (struct iphdr*)(packet + sizeof(struct ethhdr));
+		/* 
+		Create a in_addr type variable named dest_addr which will store the destination IP adress
+		tempoarily before converting it into a readable format.
+		*/
 		struct in_addr dest_addr;
+		//get the destination IP address and store it in dest_addr. Use .s_addr is used to store IPv4 addresses in network byte order
 		dest_addr.s_addr = ip_header->daddr;
+		//inet_ntoa converts the address stored in dest_addr to a human readable string and we print it out using printf
 		printf("Packet %d: IP destination address: %s\n", ++packet_count, inet_ntoa(dest_addr));
 	}
 	pcap_close(handle);
